024325c3de17d6885614785f4f934e6d694419d3
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2023-04-05 12:12:43 +0300

    [during lab 2 TH] add all test cases

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -5,7 +5,6 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import service.Service;
-import validation.StudentValidator;
 import validation.ValidationException;
 
 /**
@@ -63,6 +62,89 @@ public class AppTest
             fail("Expected an ValidationException to be thrown");
         } catch (ValidationException e) {
             assertEquals("Nume incorect!", e.getMessage());
+        }
+    }
+
+    public void testWhenGrupaIsNatural_thenOk()
+    {
+        var service = Service.getService();
+        Student student = new Student("1", "name", 1, "email");
+
+        try {
+            service.addStudent(student);
+        } catch (ValidationException e) {
+            fail("Expected no exception to be thrown");
+        }
+    }
+
+    public void testWhenGrupaIsNegative_thenFail()
+    {
+        var service = Service.getService();
+        Student student = new Student("1", "name", -1, "email");
+
+        try {
+            service.addStudent(student);
+            fail("Expected an ValidationException to be thrown");
+        } catch (ValidationException e) {
+            assertEquals("Grupa incorecta!", e.getMessage());
+        }
+    }
+
+    public void testWhenIdIsNotNull_thenOk() {
+        var service = Service.getService();
+        Student student = new Student("1", "name", 1, "email");
+
+        try {
+            service.addStudent(student);
+        } catch (ValidationException e) {
+            fail("Expected no exception to be thrown");
+        }
+    }
+
+    public void testWhenIdIsNull_thenFail() {
+        var service = Service.getService();
+        Student student = new Student(null, "name", 1, "email");
+
+        try {
+            service.addStudent(student);
+            fail("Expected an NullPointerException to be thrown");
+        } catch (NullPointerException e) {
+        }
+    }
+
+    public void testWhenIdIsEmpty_thenFail() {
+        var service = Service.getService();
+        Student student = new Student("", "name", 1, "email");
+
+        try {
+            service.addStudent(student);
+            fail("Expected an ValidationException to be thrown");
+        } catch (ValidationException e) {
+            assertEquals("Id incorect!", e.getMessage());
+        }
+    }
+
+    public void testWhenEmailIsNull_thenFail() {
+        var service = Service.getService();
+        Student student = new Student("1", "name", 1, null);
+
+        try {
+            service.addStudent(student);
+            fail("Expected an ValidationException to be thrown");
+        } catch (ValidationException e) {
+            assertEquals("Email incorect!", e.getMessage());
+        }
+    }
+
+    public void testWhenEmailIsEmpty_thenFail() {
+        var service = Service.getService();
+        Student student = new Student("2", "name", 1, "");
+
+        try {
+            service.addStudent(student);
+            fail("Expected an ValidationException to be thrown");
+        } catch (ValidationException e) {
+            assertEquals("Email incorect!", e.getMessage());
         }
     }
 }
